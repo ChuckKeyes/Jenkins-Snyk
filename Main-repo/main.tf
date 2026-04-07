535fec62ee954dbcf4981c8b4ca0d6c02d039ff7
ChuckKeyes <chuck37080@yahoo.com>
2026-04-06 21:40:41 -0400

    Update files

--- Main-repo/main.tf
+++ Main-repo/main.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "restricted_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR-IP/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
